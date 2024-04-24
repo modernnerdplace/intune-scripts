2730ded54f801e1488022d690067263cfb9b1b8d
Paul van Boerdonk <p.vanboerdonk@supervision.nl>
2024-04-24 14:34:13 +0200

    Added all script remarks, updated the script for all file extension

--- Windows/[Supervision Optimized]/SetLockscreenBackground.ps1
+++ Windows/[Supervision Optimized]/SetLockscreenBackground.ps1
@@ -1,32 +1,66 @@
-﻿$RegKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
+﻿<#
+.SYNOPSIS
+This PowerShell script is designed to change the lockscreen background image on a Windows machine. It does this by downloading an image from a specified URL, saving it to a local path, and then updating the Windows Registry to use this image as the new Lockscreen background. The script also checks and creates necessary directories and registry keys if they do not exist.
 
+.DESCRIPTION
+This PowerShell script is designed to change the lockscreen background image on a Windows machine.
+
+.EXAMPLE
+SetLockscreenBackground.ps1
+
+.PARAMETER
+None
+
+.NOTES
+File Name      : SetLockscreenBackground.ps1
+Prerequisite   : PowerShell V2.0
+#>
+
+
+# Define the registry key path and values
+$RegKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
+
+# Define the registry key values
 $LockScreenPath = "LockScreenImagePath"
 $LockScreenStatus = "LockScreenImageStatus"
 $LockScreenUrl = "LockScreenImageUrl"
 
+# Define the status value
 $StatusValue = "1"
 
+# Define the URL of the lockscreen image
 $Url = "${SUPERVISION_LOCKSCREENIMAGE}"
-$LockScreenImageValue = "C:\Windows\LockScreenImage.jpg"
+
+# Get the extension of the URL (e.g., jpg, png, etc.)
+$UrlExtension = $url.Split(".")[-1]
+
+# Define the local path of the lockscreen image
+$LockscreenImageValue = "C:\Windows\LockscreenImage.$UrlExtension"
+
+# Define the directory path
 $Directory = "C:\Windows\"
 
-
+# Check if the directory exists, if not create it
 If ((Test-Path -Path $Directory) -eq $false)
 {
 	New-Item -Path $Directory -ItemType directory
 }
 
+# Download the image from the URL and save it to the local path
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile($Url, $LockScreenImageValue)
+$wc.DownloadFile($Url, $LockscreenImageValue)
 
+# Check if the registry key path exists, if not create it
 if (!(Test-Path $RegKeyPath))
 {
 	Write-Host "Creating registry path $($RegKeyPath)."
 	New-Item -Path $RegKeyPath -Force | Out-Null
 }
 
-New-ItemProperty -Path $RegKeyPath -Name $LockScreenStatus -Value $StatusValue -PropertyType DWORD -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $LockScreenPath -Value $LockScreenImageValue -PropertyType STRING -Force | Out-Null
-New-ItemProperty -Path $RegKeyPath -Name $LockScreenUrl -Value $LockScreenImageValue -PropertyType STRING -Force | Out-Null
+# Update the registry key values
+New-ItemProperty -Path $RegKeyPath -Name $LockscreenStatus -Value $StatusValue -PropertyType DWORD -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $LockscreenPath -Value $LockscreenImageValue -PropertyType STRING -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $LockscreenUrl -Value $LockscreenImageValue -PropertyType STRING -Force | Out-Null
 
+# Update the lockscreen background
 RUNDLL32.EXE USER32.DLL, UpdatePerUserSystemParameters 1, True
